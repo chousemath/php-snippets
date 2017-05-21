47bd3548b0ea34ab1beb292c0605ed4b16400e54
Joseph Sungpil Choi <chousemath@gmail.com>
2017-05-21 18:41:19 +0900

    added a little more

--- 01_FirstPage/index.php
+++ 01_FirstPage/index.php
@@ -9,5 +9,9 @@
     <?php print('This is my first line of php'); ?>
     <br/>
     <?php print('This is my second line of php'); ?>
+    <br/>
+    <?php echo 'Hello world from echo statement!'; ?>
+    <br/>
+    <?php echo 'everything works' ?>
 	</body>
 </html>
