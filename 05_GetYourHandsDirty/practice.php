75a769b1a3387865bc6f34687c172f74cd2c9a88
Joseph Sungpil Choi <chousemath@gmail.com>
2017-05-22 19:56:20 +0900

    played around with physics equations in php

--- 05_GetYourHandsDirty/practice.php
+++ 05_GetYourHandsDirty/practice.php
@@ -1,18 +1,23 @@
 <?php
-	
+
 	// Define a Constant
-	
-	
+
 	// Your Variables
-	
-	
+	$GRAV_CONST = "6.674e−11" - 0; // gravitational constant
+  $mass1 = 10.5; // mass 1
+  $mass2 = 11.2; // mass 2
+  $dist = 0.125; // distance between the centers of the masses in m
+	$force = $GRAV_CONST * $mass1 * $mass2 / ($dist * $dist);
+  $pot_energy = -1 * $GRAV_CONST * $mass1 * $mass2 / $dist;
+  $grav_pot1 = -1 * $GRAV_CONST * $mass1 / $dist;
+  $grav_pot2 = -1 * $GRAV_CONST * $mass2 / $dist;
 	/*
 	Use PHP to calculate the difference
 	between your birth year and this year
 	to show your age dynamically
 	*/
 
-	
+
 ?>
 
 <!DOCTYPE html>
@@ -26,33 +31,33 @@
 			<a href="/" title="Back to directory" id="logo">
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
-			
+
 			<h1>Get Your Hands Dirty: <small><!-- PAGE TITLE --></small></h1>
 			<hr>
-			
+
 			<h2>Your Example</h2>
-			
-			<div class="sandbox">
-				<h3>Today's Date:</h3>
-				<p><!-- DATE --></p>
-				
-				<h3>My Name:</h3>
-				<p><!-- YOUR NAME --></p>
-				
-				<h3>My Favourite Colour:</h3>
-				<p><!-- FAVOURITE COLOUR --></p>
-				
-				<h3>My Age:</h3>
-				<p><!-- YOUR AGE --></p>
+
+      <div class="sandbox">
+				<h3>Force of Gravity:</h3>
+				<p><?php print("Force of Gravity: <b>$force</b> <i>Newtons</i>"); ?></p>
+
+        <h3>Gravitational Potential Energy</h3>
+        <p><?php print("Gravitational Potential Energy: <b>$pot_energy</b>"); ?></p>
+
+        <h3>Gravitational Potential of Mass 1</h3>
+        <p><?php print("Gravitation potential of mass 1 is <b>$grav_pot1</b>"); ?></p>
+
+        <h3>Gravitational Potential of Mass 2</h3>
+        <p><?php print("Gravitation potential of mass 2 is <b>$grav_pot2</b>"); ?></p>
 			</div><!-- end sandbox -->
-			
+
 			<a href="index.php" class="button">Back to the final example</a>
-			
+
 			<hr>
-			
+
 			<small>&copy;<!-- THIS YEAR --> - <!-- YOUR NAME --></small>
 		</div><!-- end wrapper -->
-		
+
 		<div class="copyright-info">
 			<?php include('../assets/includes/copyright.php'); ?>
 		</div><!-- end copyright-info -->
